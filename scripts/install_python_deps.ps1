3135bf46d0eac704957ea55fc201c7acc5ad4bd3
Artur <arturhapurkhaiev@gmail.com>
2026-03-09 09:52:25 +0100

    Add script to install Python dependencies

---
+++ scripts/install_python_deps.ps1
@@ -0,0 +1,7 @@
+Write-Host "Installing Python dependencies..."
+
+python -m pip install --upgrade pip
+
+python -m pip install -r C:\MiniReportServer\requirements.txt
+
+Write-Host "Python dependencies installed"
