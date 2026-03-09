d40b8e74ed8027842df9675d1cce8140c1f24ce8
Artur <arturhapurkhaiev@gmail.com>
2026-03-09 23:38:50 +0100

    Delete scripts directory

--- scripts/install_python_deps.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Installing Python dependencies..."
-
-python -m pip install --upgrade pip
-
-python -m pip install -r C:\MiniReportServer\requirements.txt
-
-Write-Host "Python dependencies installed"
