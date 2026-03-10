ff8540cbb35dad0105d187ac0dbcc6cd47ea127c
Artur <arturhapurkhaiev@gmail.com>
2026-03-10 02:29:54 +0100

    Update WSL dependencies and start Docker service
    Replace docker-compose with docker-compose-plugin and start Docker service.

--- scripts/install_wsl_deps.sh
+++ scripts/install_wsl_deps.sh
@@ -8,12 +8,20 @@ sudo apt install -y \
 git \
 make \
 docker.io \
-docker-compose \
+docker-compose-plugin \
 python3 \
 python3-pip \
 freetds-dev \
 freetds-bin \
 dos2unix
+
+echo ""
+
+echo "Starting Docker..."
+
+sudo service docker start
+
+sudo usermod -aG docker $USER
 
 echo ""
 echo "Ubuntu dependencies installed"
